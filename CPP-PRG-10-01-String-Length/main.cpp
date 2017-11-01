bbf57c8e98d14aac8b49c90987b948401eeba1ba
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-01 14:55:06 -0400

    Prompt written, initial commit.

--- CPP-PRG-10-01-String-Length/main.cpp
+++ CPP-PRG-10-01-String-Length/main.cpp
@@ -5,11 +5,17 @@
 //  Created by Keith Smith on 11/1/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a function that returns an integer and accepts a pointer to a C-string as an
+//  argument. The function should count the number of characters in the string and return that
+//  number. Demonstrate the function in a simple program that asks the user to input a
+//  string, passes it to the function, then displays the function's return value.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
     return 0;
 }
