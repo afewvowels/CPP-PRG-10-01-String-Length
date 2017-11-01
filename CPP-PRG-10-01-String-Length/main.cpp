92064bc944279838b867d44bddd3e525219ca33f
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-01 15:06:30 -0400

    Works, needs comments.

--- CPP-PRG-10-01-String-Length/main.cpp
+++ CPP-PRG-10-01-String-Length/main.cpp
@@ -11,11 +11,50 @@
 //  string, passes it to the function, then displays the function's return value.
 
 #include <iostream>
+#include <cstring>
+#include <cctype>
+#include <iomanip>
 
 using namespace std;
 
+string getUserInput();
+int *getStringLength(string *);
+
 int main()
 {
+    string *strString = nullptr;
+    int *intLength = nullptr;
+    
+    strString = new string;
+    
+    *strString = getUserInput();
+    
+    intLength = getStringLength(strString);
+    
+    cout << *intLength << endl;
     
     return 0;
+}
+
+string getUserInput()
+{
+    string *strTemp = nullptr;
+    
+    strTemp = new string;
+    
+    cout << "Please enter a string:\n";
+    getline(cin, *strTemp);
+    
+    return *strTemp;
+}
+
+int *getStringLength(string *strStr)
+{
+    int *intLen = nullptr;
+    
+    intLen = new int;
+    
+    *intLen = strStr->size();
+    
+    return intLen;
 }
